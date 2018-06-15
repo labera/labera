ebf65d4ac97e0cda31c076c4e6deb5bc56f9153e
Archna <archnapivotal@gmail.com>
2018-06-15 22:28:49 +0530

    Login final

---
+++ build/resources/main/db/migration/V1__Create_User_Table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user_master
+     (
+       user_id              VARCHAR(150) NOT NULL,
+       user_name            VARCHAR(150) ,
+       password        VARCHAR(150) NOT NULL,
+       branch_id            VARCHAR(150) NOT NULL,
+       PRIMARY KEY     (user_id)
+     );
